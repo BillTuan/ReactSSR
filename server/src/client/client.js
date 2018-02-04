7d09f20f23ff74cfa5d26fdc7212be2b3f267600
Bill <baotuan0.0@gmail.com>
2018-02-04 21:51:05 +0700

    Added routing for client side

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from "./components/Home";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
 
+const App = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 /* eslint no-undef: 0 */
-ReactDOM.hydrate(<Home />, document.querySelector("#root"));
+ReactDOM.hydrate(<App />, document.querySelector("#root"));
