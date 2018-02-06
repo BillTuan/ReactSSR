fe5a94c4c4fb3677f98fdb018dcfe87f9092627c
Bill <baotuan0.0@gmail.com>
2018-02-06 15:24:41 +0700

    Change route setting with router config

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { renderRoutes } from "react-router-config";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 import Routes from "./Routes";
@@ -13,7 +14,7 @@ const store = createStore(reducers, {}, applyMiddleware(thunk));
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <Routes />
+      <div>{renderRoutes(Routes)}</div>
     </BrowserRouter>
   </Provider>
 );
