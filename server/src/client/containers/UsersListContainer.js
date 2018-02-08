37716e5e7e522917f224977c2719a7df62e23900
Bill <baotuan0.0@gmail.com>
2018-02-08 18:30:23 +0700

    Added helmet to make meta tag

--- server/src/client/containers/UsersListContainer.js
+++ server/src/client/containers/UsersListContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 import { fetchUsers } from "../actions";
 
 class UsersList extends Component {
@@ -13,12 +14,21 @@ class UsersList extends Component {
     this.props.fetchUsers();
   }
 
+  head = () => (
+    <Helmet>
+      <title>{`${this.props.users.length} User App`}</title>
+      <meta property="og:title" content="User App" />
+    </Helmet>
+  );
+
   renderUsers() {
     return this.props.users.map(({ id, name }) => <li key={id}>{name}</li>);
   }
+
   render() {
     return (
       <div>
+        {this.head()}
         Here us a big list of users:
         <ul>{this.renderUsers()}</ul>
       </div>
